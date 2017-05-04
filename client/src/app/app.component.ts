00ac0e2a37317bb0272da49277ebf30b80f39c46
Ibro <ibro@ibro.me>
2017-05-04 08:19:56 +0200

    injecting ChatService to app.component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../chat.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  
+  constructor(chatService: ChatService) { }
 }
