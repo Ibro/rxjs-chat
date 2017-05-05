3b67f9a2c5fd9422de685f8bdb0e1a3976227cad
Ibro <ibro@ibro.me>
2017-05-05 08:27:11 +0200

    ChatService - using socket.emit to send new message

--- client/src/chat.service.ts
+++ client/src/chat.service.ts
@@ -2,9 +2,13 @@ import * as io from 'socket.io-client';
 
 export class ChatService {
     private url = 'http://localhost:3000';
-    private socket;    
+    private socket;
 
     constructor() {
         this.socket = io(this.url);
+    }
+
+    public sendMessage(message) {
+        this.socket.emit('new-message', message);
     }
 }
