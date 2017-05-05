3ee21e9188f1d15790cd8650927a0e52038cc83e
Ibro <ibro@ibro.me>
2017-05-05 20:08:20 +0200

    subscribing to observable from AppComponent, pushing new messages to this.messages

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { ChatService } from '../chat.service';
 })
 export class AppComponent {
   message: string;
+  messages: string[] = [];
 
   constructor(private chatService: ChatService) {
   }
@@ -15,5 +16,13 @@ export class AppComponent {
   sendMessage() {
     this.chatService.sendMessage(this.message);
     this.message = '';
+  }
+
+  ngOnInit() {
+    this.chatService
+      .getMessages()
+      .subscribe((message: string) => {
+        this.messages.push(message);
+      });
   }
 }
